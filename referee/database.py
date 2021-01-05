abff81ea070cb8e6b75a8bb66ccd2de459fc26d0
FedeClaudi <federicoclaudi@protonmail.com>
2021-01-05 20:02:13 +0000

    bump

--- referee/database.py
+++ referee/database.py
@@ -4,7 +4,9 @@ from pathlib import Path
 import gzip
 import multiprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 from loguru import logger
+import numpy as np
 
 from .settings import fields_of_study, base_dir, low_year, keywords
 from .utils import isin, to_txt
@@ -12,7 +14,7 @@ from ._dbase import load_abstracts
 
 
 def exclude(entry):
-    '''
+    """
         Only select papers based on:
             * field of study
             * publication data
@@ -25,23 +27,22 @@ def exclude(entry):
 
         Returns:
             exclude: bool. True if the entry fails any of the criteria
-    '''
+    """
     # keep only entries in relevant fields
-    if not isin(entry['fieldsOfStudy'], fields_of_study):
+    if not isin(entry["fieldsOfStudy"], fields_of_study):
         return True
 
     # Keep only recent papers
-    entry['year'] = entry['year'] or 0
-    if entry['year'] < low_year: 
+    entry["year"] = entry["year"] or 0
+    if entry["year"] < low_year:
         return True
 
     # keep only entries with keywords in abstract
-    if not any((keyword in entry['paperAbstract']) for keyword in keywords):
+    if not any((keyword in entry["paperAbstract"]) for keyword in keywords):
         return True
 
     # ok all good
     return False
-
 
 
 def _parse_single_file(args):
@@ -49,30 +50,25 @@ def _parse_single_file(args):
     logger.debug(f"Parsing compressed file: {fpath.name}")
 
     # check if file was opened before
-    name = fpath.name.split('.')[0]
-    out = dfs_dir / (name + '.h5')
+    name = fpath.name.split(".")[0]
+    out = dfs_dir / (name + ".h5")
     # if out.exists():
     #     return pd.read_hdf(out, key='hdf')
 
     # load data
-    if fpath.suffix == '.gz':
-        with gzip.open(fpath,'r') as datafile:
+    if fpath.suffix == ".gz":
+        with gzip.open(fpath, "r") as datafile:
             data = datafile.readlines()
-        data = [d.decode('utf-8') for d in data]
+        data = [d.decode("utf-8") for d in data]
     else:
         with open(fpath) as datafile:
             data = datafile.readlines()
 
     # create a dataframe with relevant data
     metadata = dict(
-        title=[],
-        authors = [],
-        doi=[],
-        url=[],
-        field_of_study=[],
-        id=[]
+        title=[], authors=[], doi=[], url=[], field_of_study=[], id=[]
     )
-    
+
     # loop over all entries
     for entry in data:
         entry = json.loads(entry)
@@ -81,25 +77,27 @@ def _parse_single_file(args):
             continue
 
         # save abstract to file
-        to_txt(str(entry['paperAbstract']), abstracts_dir / f'{entry["id"]}.txt')
+        to_txt(
+            str(entry["paperAbstract"]), abstracts_dir / f'{entry["id"]}.txt'
+        )
 
         # keep metadata
-        metadata['id'].append(str(entry['id']))
-        metadata['title'].append(str(entry['title']))
-        metadata['authors'].append([str(a['name']) for a in entry['authors']])
-        metadata['doi'].append(entry['doi'] or '')
-        metadata['url'].append(entry['s2Url'] or '')
-        metadata['field_of_study'].append(entry['fieldsOfStudy'] or [''])
+        metadata["id"].append(str(entry["id"]))
+        metadata["title"].append(str(entry["title"]))
+        metadata["authors"].append([str(a["name"]) for a in entry["authors"]])
+        metadata["doi"].append(entry["doi"] or "")
+        metadata["url"].append(entry["s2Url"] or "")
+        metadata["field_of_study"].append(entry["fieldsOfStudy"] or [""])
 
     metadata = pd.DataFrame(metadata)
 
-    # save 
-    print(f'Processed {n}/{N} kept {len(metadata)}/{len(data)} papers')
-    metadata.to_hdf(out, key='hdf')
+    # save
+    print(f"Processed {n}/{N} kept {len(metadata)}/{len(data)} papers")
+    metadata.to_hdf(out, key="hdf")
 
 
 def upack_database(folder):
-    '''
+    """
         Opens up .gz with papers metadata info from 
         http://s2-public-api-prod.us-west-2.elasticbeanstalk.com/corpus/download/
         and saves selected papers to dataframes and their abstracts to .txt
@@ -114,32 +112,35 @@ def upack_database(folder):
         Arguments:
             folder: str, Path. Path to the folder where the database data will be stored.
                 It must include a subfolder called 'compressed' in which the .gz files live. 
-    '''
-    loguru.debug(f'Unpacking database in {folder}')
+    """
+    logger.debug(f"Unpacking database in {folder}")
 
     # get folders
     folder = Path(folder)
-    
-    dfs_dir = folder / 'dfs'
+
+    dfs_dir = folder / "dfs"
     dfs_dir.mkdir(exist_ok=True)
 
-    abstracts_dir = folder / 'abstracts'
+    abstracts_dir = folder / "abstracts"
     abstracts_dir.mkdir(exist_ok=True)
 
     # extract data from all files
-    files = list((folder/'compressed').glob('*.gz'))
+    files = list((folder / "compressed").glob("*.gz"))
 
     # for debugging
     # _parse_single_file((files[0], abstracts_dir, dfs_dir, 0, len(files)))
 
     n_cpus = multiprocessing.cpu_count()
     with multiprocessing.Pool(processes=n_cpus) as pool:
-        args = [(fl, abstracts_dir, dfs_dir, n, len(files)) for n, fl in enumerate(files)]
-        results = pool.map(_parse_single_file, args)
+        args = [
+            (fl, abstracts_dir, dfs_dir, n, len(files))
+            for n, fl in enumerate(files)
+        ]
+        pool.map(_parse_single_file, args)
 
 
 def make_database(folder):
-    '''
+    """
         Given a database folder filled in by `unpack_database` this function creates the database proper. 
         It loads the dataframes and abstracts saved by `unpack_database` and uses 
         erm Frequency-Inverse Document Frequency (TF-IDF) embedding
@@ -149,52 +150,45 @@ def make_database(folder):
         Arguments:
             folder: str, Path. Path to the folder where the database data is stored.
                 User must have run `unpack_database` on the folder's content first. 
-    '''
+    """
     logger.debug(f"Making database in folder: {folder}")
 
     folder = Path(folder)
-    abstracts_dir = folder / 'abstracts'
+    abstracts_dir = folder / "abstracts"
 
-    files = (folder / 'dfs').glob('*.h5')
+    files = (folder / "dfs").glob("*.h5")
 
-    # Get the ID of each paper
-    ids = []
+    # Load all metadata into a single dataframe
+    dfs = []
     for f in files:
-        ids.extend(list(pd.read_hdf(f, key='hdf')['id'].values))
+        dfs.append(pd.read_hdf(f, key="hdf")["id"].values)
+    DATA = pd.concat(dfs)
+    ids = DATA["id"]
     logger.debug(f"Found {len(ids)} papers")
 
-    ids = ids[:10] # ! for debugging
+    ids = ids[:10]  # ! for debugging
 
     # Load each paper's abstract to create embedding
-    logger.debug('Loading abstracts')
+    logger.debug("Loading abstracts")
     abstracts = load_abstracts(ids, abstracts_dir)
 
     # create embedding
     # Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
-    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf = TfidfVectorizer(stop_words="english")
 
     # Construct the required TF-IDF matrix by fitting and transforming the data
-    logger.debug('Fitting TfidfVectorizer model')
+    logger.debug("Fitting TfidfVectorizer model")
     tfidf_matrix = tfidf.fit_transform(abstracts)
 
     # compute cosine similarity
+    similarity = linear_kernel(tfidf_matrix, tfidf_matrix)
+    logger.debug(
+        f'Saving similarity matrix with shape: {similarity.shape} at {base_dir/"similarity.npy"}'
+    )
 
-    
     # save results
-
-    a = 1
-    # dfs = []
-    # for n, fl in track(enumerate(files), description='opening compressed', total=len(files)):
-    #     dfs.append(_parse_single_file(fl, abstracts_dir, dfs_dir))
-
-    # # save all dfs
-    # pd.concat(dfs).to_hdf(folder/'database.h5', key='hdf')
-    # a = 1
-
-    # compute Term Frequency-Inverse Document Frequency
-    # embedding for terms in the abstracts
-
-    # compute cosine-similarity score
-
-    # save metadata and similarity matrix to file
-    return
+    np.save(str(base_dir / "similarity.npy"), similarity)
+    DATA.to_hdf(base_dir / "database.h5", key="hdf")
+    logger.debug(
+        f'Saved database at: {base_dir/"database.h5"}. {len(DATA)} entries in total'
+    )
